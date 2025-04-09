ef503d9111ee1085d6d7860856ba3d14ebd76d06
Soren Rasmussen <s.c.rasmussen@gmail.com>
2025-04-09 15:10:40 -0600

    Initial mpas-atm nuopc files, mostly stubs for init, step, and finalize

---
+++ src/core_atmosphere/couple/nuopc/mpas_atm_nuopc_gluecode.F90
@@ -0,0 +1,11 @@
+module mpas_atm_nuopc_gluecode
+! This module connects NUOPC initialize, advance, and finalize to mpas-atm.
+
+  use esmf
+  use nuopc
+  ! use nuopc_model!, only: model_routine_SS => SetServices
+  implicit none
+
+contains
+
+end module mpas_atm_nuopc_gluecode
